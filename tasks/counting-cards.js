811e1ab82b40cc6da38488411386081db1720e28
Alexandr <yaromahob@gmail.com>
2022-08-24 23:14:37 +0300

    feat: task solved

--- tasks/counting-cards.js
+++ tasks/counting-cards.js
@@ -17,3 +17,41 @@ Do NOT reset count to 0 when value is 7, 8, or 9.
 Do NOT return an array.
 Do NOT include quotes (single or double) in the output.
  */
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
+  }
+
+  if (count < 0) {
+    return `${count} Hold`;
+  } else if (count == 0) {
+    return `${count} Hold`;
+  } else return `${count} Bet`;
+
+  // Only change code above this line
+}
+
+cc(2);
+cc(3);
+cc(7);
+cc('K');
+cc('A');
