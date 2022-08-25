dcb845d2c282564d4d090a1c7c74af6b0e74a076
Alexandr <yaromahob@gmail.com>
2022-08-25 19:57:10 +0300

    feat: task solved

---
+++ tasks/AccObjPropWithBracketNotation.js
@@ -0,0 +1,12 @@
+/**
+ * Read the values of the properties an entree and the drink of testObj using bracket notation and assign them to entreeValue and drinkValue respectively.
+ */
+
+const testObj = {
+  'an entree': 'hamburger',
+  'my side': 'veggies',
+  'the drink': 'water',
+};
+
+const entreeValue = testObj['an entree'];
+const drinkValue = testObj['the drink'];
