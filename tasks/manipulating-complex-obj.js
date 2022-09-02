421b0a96f91019766c3e9d64b4ac2b073585df40
Alexandr <yaromahob@gmail.com>
2022-09-02 21:09:42 +0300

    task solved

---
+++ tasks/manipulating-complex-obj.js
@@ -0,0 +1,25 @@
+/* Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.*/
+const myMusic = [
+	{
+		"artist": "Billy Joel",
+		"title": "Piano Man",
+		"release_year": 1973,
+		"formats": [
+			"CD",
+			"8T",
+			"LP"
+		],
+		"gold": true
+	},
+	{
+		"artist": "Daft Punk",
+		"title": "Homework",
+		"release_year": 1997,
+		"formats": [
+			"CD",
+			"Cassette",
+			"LP"
+		],
+		"gold": true
+	}
+];
